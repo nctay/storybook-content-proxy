e5a772c2a76f2f606f3706a0ce3e69110e0fb5a5
DVFrolov <dvfrolov@mtsbank.ru>
2020-03-18 14:54:19 +0300

    proxy added

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express')
-const port = process.env.PORT || 3000;
-const app = express();
+const port = process.env.PORT || 3000
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
-app.get('/', function (req, res) {
+const app = express()
+
+// proxy middleware options
+const options = {
+    target: 'https://www.mtsbank.ru', // target host
+    changeOrigin: true, // needed for virtual hosted sites
+    headers: {
+        Connection: 'keep-alive',
+        "Access-Control-Allow-Origin": "*",
+        https: true
+    },
+};
+
+// create the proxy (without context)
+const exampleProxy = createProxyMiddleware(options);
+
+app.use('/upload', exampleProxy)
+app.get('/hello', function (req, res) {
     res.send(JSON.stringify({ Hello: 'World'}));
 });
 
